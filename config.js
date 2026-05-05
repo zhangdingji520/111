1f6bc5d3e4a35a3a9a6b460c9d94ef91ac95dc32
zhangdingji520 <146807726+zhangdingji520@users.noreply.github.com>
2026-05-06 04:20:12 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// 引入 Supabase SDK (通过 CDN)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-const supabaseUrl = 'https://hntioocpuhbrwiuvdify.supabase.co';
-const supabaseKey = 'EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudGlvb2NwdWhicndpdXZkaWZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTUwNDIsImV4cCI6MjA5MzU3MTA0Mn0.3kLSZV1scyC89dWVXPi0PZAg7jWVUDG6psHLNUKgvKo';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
